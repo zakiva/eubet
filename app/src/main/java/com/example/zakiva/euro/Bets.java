4714bc8a75048f70fc3ca18c6f6ed4d61dff5b7c
Zahi Akiva <zakiva@redhat.com>
2016-06-05 19:37:36 +0300

    Add icons and design main screen

--- app/src/main/java/com/example/zakiva/euro/Bets.java
+++ app/src/main/java/com/example/zakiva/euro/Bets.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Bets extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class Bets extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bets);
+        ((ImageView) findViewById(R.id.game_icon)).bringToFront();
+
     }
 
     public void buttonWinningTeamClicked(View view) {
